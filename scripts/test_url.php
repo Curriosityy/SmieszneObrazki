29e2447da80b740d31e11ed8ad256210fcdbbc06
Curriosityy <patryk9595@gmail.com>
2018-11-11 18:59:18 +0100

    -added php script to check if posted url is a photo

---
+++ scripts/test_url.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+if($_SERVER['REQUEST_METHOD']!='POST'){
+  $_SESSION["error"]="Brak dostępu";
+  header("location: ../index.php");
+  exit;
+}else{
+if(@getimagesize($_POST["imgUrl"]))
+{
+  echo 'isImage';
+}else{
+  echo 'notImage';
+}
+}
+?>
