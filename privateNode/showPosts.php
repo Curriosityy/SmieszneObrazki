cf6eb9f7a3333c094ea0cbe890c6a31e09b4d900
Curriosityy <patryk9595@gmail.com>
2018-11-21 16:40:47 +0100

    -added comment counter

--- privateNode/showPosts.php
+++ privateNode/showPosts.php
@@ -1,14 +1,15 @@
 <?php
-    function printText($userRow,$textRow,$color)
+    function printText($userRow,$textRow,$color,$commentsCount)
     {
+      $commentsCount=$commentsCount==-1?"":"<span class='badge'>$commentsCount</span>";
       $userLogin=$textRow["anon"]==1?'Anonim':$userRow["login"];
       $userImage=$userRow["image"];
-      $obrazek=$textRow['image']==''?'':"<img class='img-rounded obrazekImg img-responsive' src='".$textRow['image']."' alt='obrazekPostu'>";
+      $obrazek=$textRow['image']==''?'':"<img class='img-rounded img-responsive' src='".$textRow['image']."' alt='obrazekPostu'>";
       $profile=$textRow['anon']==1?"<img class='img-circle' height='55' src='anon.png' alt='miniaturka'>":"<a href='profil.php?profile=".$userLogin."' style='padding:1px;'><img class='img-circle' height='55' src='".$userImage."' alt='miniaturka'></a>";
       $butoniki=$_SESSION["logged"]==true?"
       <div class='row'>
         <a class='col-sm-3 przycisk'>Lubie To <span class='glyphicon glyphicon-thumbs-up'></span></a>
-        <a class='col-sm-3 przycisk'>Komentuj <span class='glyphicon glyphicon-comment'></span></a>
+        <a class='col-sm-3 przycisk'>Komentuj <span class='glyphicon glyphicon-comment'></span> ".$commentsCount."</a>
         <a class='col-sm-3 przycisk'>Dodaj do ulubionych <span class='glyphicon glyphicon-heart-empty'></span></a>
         <a class='col-sm-3 przycisk'>Zgłoś <span class='glyphicon glyphicon-warning-sign'></span></a>
         <input type='hidden' name='anonPosting' value='postId".$textRow['id']."'>
@@ -20,14 +21,10 @@
           ".$profile."
         </div>
         <div class='media-body'>
-          <h4 class'media-heading'><a href='profil.php?profile=".$userLogin."' style='padding:1px;'></a>".$userLogin."<small> ".$textRow['create_date']."</small></h4>
+          <h4 class='media-heading'><a href='profil.php?profile=".$userLogin."' style='padding:1px;'></a>".$userLogin."<small> ".$textRow['create_date']."</small></h4>
           <p>".$textRow['tekst']."</p>
           ".$obrazek."\n
         <hr style='margin-top:5px;margin-bottom:5px'>".$butoniki."";
-      if($color=="")
-      {
-        echo "</div></div>";
-      }
     }
     if(isset($_GET['page']))
     {
@@ -53,14 +50,15 @@
             $prep2->execute([':id'=>$row['user_id']]);
             $row2=$prep2->fetch();
             $color=$i%2==0?'lighter row':'darker row';
-            printText($row2,$row,$color);
             $prep3=$pdo->prepare("SELECT id,tekst, image, up_vote, user_id, post_id, create_date, anon FROM posts WHERE post_id=:post_id AND accepted=1");
             $prep3->execute([":post_id"=>$row['id']]);
+            printText($row2,$row,$color,$prep3->rowCount());
             while($row3=$prep3->fetch()){
               $prep4=$pdo->prepare("SELECT login, image FROM users WHERE id=:id");
               $prep4->execute([':id'=>$row3['user_id']]);
               $row4=$prep4->fetch();
-              printText($row4,$row3,"");
+              printText($row4,$row3,"",-1);
+              echo "</div></div>";
             }
             echo"
                 </div>
